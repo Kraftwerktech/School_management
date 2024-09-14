953a11c07b1611edbc542d3239a6e1bcadd43718
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2024-09-14 18:59:28 +0600

    update

--- dashboard/src/components/layout/index.tsx
+++ dashboard/src/components/layout/index.tsx
@@ -1,14 +1,11 @@
 import type { PropsWithChildren } from "react";
-import { Breadcrumb } from "../breadcrumb";
 import { Menu } from "../menu";
-
 export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="layout">
+    <div>
       <Menu />
       <div className="content">
-        <Breadcrumb />
-        <div>{children}</div>
+        <div className="ml-[270px]">{children}</div>
       </div>
     </div>
   );
